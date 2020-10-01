8671376efc72b6c8c2a4f61f9e7de5f7b5ec155f
Mario Alves de Souza Carvalho <44899267+MarioAlvesCarvalho@users.noreply.github.com>
2020-10-01 08:51:15 -0300

    Update Install_veianew_docker.sh

--- Install_veianew_docker.sh
+++ Install_veianew_docker.sh
@@ -6,15 +6,15 @@ unset VEIANEW &&
 export APACHE2="/usr/local/apache22" &&
 export VEIANEW="${APACHE2}/VEIANEW" &&
 wget -N -v --tries=1 -P ${VEIANEW} https://github.com/MarioAlvesCarvalho/veianew/archive/master.zip &&
-chmod -R 777 ${VEIANEW}/master.zip &&
-chmod +x ${VEIANEW}/master.zip &&
+chmod -R 777 ${VEIANEW}/master.zip ;
+chmod +x ${VEIANEW}/master.zip ;
 unzip -oq ${VEIANEW}/master.zip -d ${VEIANEW} &&
 sleep 1 &&
 rm -rf ${VEIANEW}/*.sh ;
-cp -rf ${VEIANEW}/veianew-master/* ${VEIANEW}/ &&
-chmod 775 ${VEIANEW}/*.sh ${VEIANEW}/*.vnw &&
-chown root.root ${VEIANEW}/*.sh ${VEIANEW}/*vnw &&
-rm -rf ${VEIANEW}/master.zip ${VEIANEW}/veianew-master &&
+cp -rf ${VEIANEW}/veianew-master/* ${VEIANEW}/ ;
+chmod 775 ${VEIANEW}/*.sh ${VEIANEW}/*.vnw ;
+chown root.root ${VEIANEW}/*.sh ${VEIANEW}/*vnw ;
+rm -rf ${VEIANEW}/master.zip ${VEIANEW}/veianew-master ;
 echo -e "" &&
 echo -e "" &&
 echo -e "" &&
